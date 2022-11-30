eb4e584cdd02d4832de039bc9b54637f857ca924
Maunish Lankapati <monu.lankapati61@gmail.com>
2022-11-30 01:25:39 -0500

    resolved merge conficlts and initial commit

--- routes/loginandreg.js
+++ routes/loginandreg.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const index = require('../data/index');
-const validate = require("../helpers");
 
 router.route('/')
 .get(async (req, res) => {
@@ -31,12 +30,9 @@ router.route('/sign-in')
         let emailId = req.body.emailIdInput;
         let password = req.body.passwordInput;
         let userType = req.body.userType; 
-        validate.validateUser(emailId, password);
-        emailId = emailId.trim().toLowerCase();
-        password = password.trim();
         if(userType == 'owner') await index.owner.checkUser(emailId, password);
         else await index.student.checkUser(emailId, password);
-        req.session.user = {emailId: emailId, userType: userType};
+        req.session.user = {emailId: emailId};
         res.redirect('/properties');
     }catch(e) {
         res.status(404).render('./sign-in_page', {title: "Sign-in Page", error: e})
@@ -65,31 +61,13 @@ router.route('/sign-up')
         let city = req.body.city;
         let state = req.body.state;
         let age = req.body.age;
-        let userType = req.body.userType;
-        validate.validateRegistration(emailId,password,firstName,lastName,contact,gender,city,state,age);
-        emailId=emailId.trim().toLowerCase();
-        password=password.trim();
-        firstName=firstName.trim();
-        lastName=lastName.trim();
-        contact=contact.trim();
-        gender=gender.trim();
-        city=city.trim();
-        state=state.trim();
-        age=age.trim(); 
+        let userType = req.body.userType; 
         if(userType == 'owner') await index.owner.createUser(emailId, password, firstName, lastName, contact, gender, city, state, age);
         else await index.student.createUser(emailId, password, firstName, lastName, contact, gender, city, state, age);
-        res.redirect('/properties');
+        res.redirect('/sign-in');
     }catch(e) {
         res.status(404).render('./sign-up_page', {title: "Sign-up Form", error: e})
     }
-})
-
-router.route('/logout')
-.get(async (req, res) => {
-    try {
-        req.session.destroy();
-        return res.render('./logout_page', {title: "Logout Page"});
-    }catch(e) {}
 })
 
 module.exports = router;
