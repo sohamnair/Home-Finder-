baaf85781e06e5db0b041f6faaa9354944fa7d5e
Maunish Lankapati <monu.lankapati61@gmail.com>
2022-12-18 00:08:55 -0500

    Added input validation for comment

--- public/comments.js
+++ public/comments.js
@@ -4,11 +4,16 @@ let errorDiv = document.getElementById('empty-comment');
 
 form.addEventListener('submit', (event) => {
    event.preventDefault();
-   if(!commentInput.value) {
+   if(!commentInput.value || !/\S/.test(commentInput.value)) {
        errorDiv.hidden = false;
        errorDiv.innerHTML = 'Comment cannot be empty!';
+       form.reset();
        errorDiv.focus();
    }
    else
        errorDiv.hidden = true;
 });
+
+commentInput.addEventListener('input', ()=>{
+    errorDiv.hidden = true
+})
